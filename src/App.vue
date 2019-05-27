b4f11356bead538695385bd1c297cf19a45bec6d
Avechi Dev 1 <kipkirui42@gmail.com>
2019-05-27 15:30:36 +0300

    Testing Repo

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos = "todos"></Todos>
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+
+  data()
+  {
+    return{
+      todos: 
+      [
+        {
+          id: 1,
+          title: "To do one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To do two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "To do three",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+*
+{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body
+{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
